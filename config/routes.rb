9ebdfa0deeeb4e207906d270e9a3dc86f921dd2e
Suman Puri <sumanpuri55@gmail.com>
2016-03-26 18:01:26 +0530

    update root page for logged in user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+
   devise_for :users
-  resources :posts
+  authenticated :user do
+    root 'posts#index', as: :authenticated_root
+  end
+
   root 'home#index'
+  resources :posts
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
