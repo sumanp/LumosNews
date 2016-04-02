e87db65a533893ebc953df8b5d7e5f497fcf6c07
Suman Puri <sumanpuri55@gmail.com>
2016-04-02 19:57:35 +0530

    fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :name, presence: true
+  
   acts_as_voter
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
